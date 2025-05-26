608fee3096db0d348919b7333b5a04d17757e731
gannashalaby <gannatony2002@gmail.com>
2025-05-26 21:53:58 +0300

    solved issues regarding gradle suitable java version

--- flutter_application_1/android/build.gradle.kts
+++ flutter_application_1/android/build.gradle.kts
@@ -5,13 +5,7 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
+/* Removed custom build directory configuration to avoid Gradle incompatibility issues */
 subprojects {
     project.evaluationDependsOn(":app")
 }
