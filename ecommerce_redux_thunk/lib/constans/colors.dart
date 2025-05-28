85d3ec7d975c2f4da33238277403efe61b5f4aec
gannashalaby <gannatony2002@gmail.com>
2025-05-28 18:55:01 +0300

    some global custom colors practicings

---
+++ ecommerce_redux_thunk/lib/constans/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color bodyColor = Color.fromARGB(255, 90, 189, 255);
+  static const Color backgroundColor = Color(0xFFF5F5F5);
+  static const Color textColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color successColor = Color.fromARGB(255, 7, 11, 255);
+  static const Color errorColor = Color(0xFFB00020);
+}
