d2bea283eb9527f3462490076d942df307c9dd7e
gannashalaby <gannatony2002@gmail.com>
2025-06-04 21:25:38 +0300

    loginscreen has to remove user id from all of the app, need to be fixed

--- ecommerce_redux_thunk/lib/redux/middleware/user_thunk.dart
+++ ecommerce_redux_thunk/lib/redux/middleware/user_thunk.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_thunk/redux_thunk.dart';
 import 'package:ecommerce_redux_thunk/models/user_model.dart';
@@ -30,21 +29,14 @@ ThunkAction<UserState> registerUserThunk(String name, String password) {
   };
 }
 
-ThunkAction<UserState> loginUserThunk(String name, String password) {
+ThunkAction<UserState> loginUserThunk(int id, String name, String password) {
   return (Store<UserState> store) async {
     store.dispatch(LoginUserRequest());
 
     try {
-      final users = await userService.getAllUsers();
+      // final users = await userService.getAllUsers();
 
-      final user = users.userList.firstWhere(
-        (u) => u.name == name,
-        orElse: () => throw Exception('Username does not exist!'),
-      );
-
-      if (user.password != password) {
-        throw Exception("Password is incorrect!");
-      }
+      final user = User(id: id, name: name, password: password);
 
       store.dispatch(LoginUserSuccess(user));
     } catch (e) {
--- ecommerce_redux_thunk/lib/screens/login_screen.dart
+++ ecommerce_redux_thunk/lib/screens/login_screen.dart
@@ -4,6 +4,10 @@ import 'package:ecommerce_redux_thunk/services/user_service.dart';
 import 'package:ecommerce_redux_thunk/constans/colors.dart';
 import 'package:ecommerce_redux_thunk/constans/texts.dart';
 import 'package:ecommerce_redux_thunk/screens/home_screen.dart';
+import 'package:redux/redux.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:ecommerce_redux_thunk/redux/states/user_state.dart';
+import 'package:ecommerce_redux_thunk/redux/middleware/user_thunk.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = '/login';
@@ -15,141 +19,205 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final UserService _userService = UserService();
+  bool _isLoading = false;
+  String? _error;
 
-  Future<void> _loginUser() async {
+  void _submit(Store<UserState> store) async {
+    final int id = 0;
     final String name = _usernameController.text.trim();
     final String password = _passwordController.text.trim();
+    if (!_formKey.currentState!.validate()) return;
 
-    if (name.isEmpty || password.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Please enter both username and password')),
-      );
-      return;
-    }
+    setState(() {
+      _isLoading = true;
+      _error = null;
+    });
 
     final users = await _userService.getAllUsers();
 
-    final usernameExists = users.userList.any((user) => user.name == name && user.password == password);
+    final noUsernameExists = users.userList.any((user) => user.name != name);
     final wrongPassword = users.userList.any((user) => user.name == name && user.password != password);
 
-    if (usernameExists) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('You are logged in successfully!')),
-      );
-    } else if (wrongPassword) {
+    if (wrongPassword) {
+      setState(() {
+        _isLoading = false;
+      });
+
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Password is incorrect 😕!')),
       );
       return;
-    } else {
+    } else if (noUsernameExists) {
+      setState(() {
+        _isLoading = false;
+      });
+
       ScaffoldMessenger.of(
         context,
       ).showSnackBar(const SnackBar(content: Text('Username does not exist!')));
       return;
     }
 
-    Navigator.pushNamed(context, HomeScreen.id);
+    try {
+      await store.dispatch(
+        loginUserThunk(id, _usernameController.text, _passwordController.text),
+      );
+
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Logged in successfully!")),
+        );
+
+        Navigator.pushReplacementNamed(context, HomeScreen.id);
+      }
+    } catch (e) {
+      setState(() {
+        _error = "Logging in failed: ${e.toString()}";
+      });
+    } finally {
+      if (mounted) {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Log In Screen'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: Column(
-            children: [
-              TextField(
-                controller: _usernameController,
-                style: TextStyle(color: CustomColors.backgroundColor),
-                decoration: const InputDecoration(
-                  labelText: 'Username',
-                  labelStyle: CustomTextStyles.buttonText,
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: CustomColors.backgroundColor),
+    return StoreConnector<UserState, Store<UserState>>(
+      converter: (store) => store,
+      builder: (context, store) {
+        return Scaffold(
+          appBar: AppBar(title: const Text('Log In Screen')),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  if (_error != null)
+                    Text(_error!, style: CustomTextStyles.errorTextStyle),
+                  TextFormField(
+                    controller: _usernameController,
+                    style: TextStyle(color: CustomColors.backgroundColor),
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                      labelStyle: CustomTextStyles.buttonText,
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: CustomColors.backgroundColor,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: CustomColors.backgroundColor,
+                        ),
+                      ),
+                    ),
+                    validator: (value) =>
+                        value!.isEmpty ? 'Please enter username' : null,
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: CustomColors.backgroundColor),
+                  const SizedBox(height: 12),
+                  TextFormField(
+                    controller: _passwordController,
+                    style: TextStyle(color: CustomColors.backgroundColor),
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: CustomTextStyles.buttonText,
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: CustomColors.backgroundColor,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: CustomColors.backgroundColor,
+                        ),
+                      ),
+                    ),
+                    obscureText: true,
+                    validator: (value) =>
+                        value!.isEmpty ? 'Please enter password' : null,
                   ),
-                ),
-              ),
-              TextField(
-                controller: _passwordController,
-                style: TextStyle(color: CustomColors.backgroundColor),
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  labelStyle: CustomTextStyles.buttonText,
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: CustomColors.backgroundColor),
+                  const SizedBox(height: 24),
+                  _isLoading
+                      ? const CircularProgressIndicator()
+                      : ElevatedButton(
+                          onPressed: () => _submit(store),
+                          child: const Text(
+                            'Login',
+                            style: CustomTextStyles.buttonText,
+                          ),
+                        ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Don\'t have an account?',
+                    style: CustomTextStyles.caption,
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: CustomColors.backgroundColor),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, RegisterScreen.id);
+                    },
+                    child: const Text(
+                      'Signup',
+                      style: CustomTextStyles.buttonText,
+                    ),
                   ),
-                ),
-                obscureText: true,
+                ],
               ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: _loginUser,
-                child: const Text('Login', style: CustomTextStyles.buttonText),
-              ),
-              const SizedBox(height: 20),
-              Text('Don\'t have an account?', style: CustomTextStyles.caption),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, RegisterScreen.id);
-                },
-                child: const Text('Sigup', style: CustomTextStyles.buttonText),
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
-      bottomNavigationBar: Container(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                await _userService.printJsonContent();
-              },
-              child: const Text(
-                'Print Users',
-                style: CustomTextStyles.smallButtonText,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                await _userService.clearAllUsers();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('All users deleted.')),
-                );
-              },
-              child: const Text(
-                'Clear Users',
-                style: CustomTextStyles.smallButtonText,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                await _userService.getJsonFilePath();
-              },
-              child: const Text(
-                'Print Users Path',
-                style: CustomTextStyles.smallButtonText,
-              ),
-            ),
-          ],
-        ),
-      ),
+          bottomNavigationBar: StoreConnector<UserState, Store<UserState>>(
+            converter: (store) => store,
+            builder: (context, store) {
+              return Container(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        store.dispatch(printUsersThunk());
+                      },
+                      child: const Text(
+                        'Print Users',
+                        style: CustomTextStyles.smallButtonText,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        store.dispatch(clearUsersThunk());
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('All users deleted.')),
+                        );
+                      },
+                      child: const Text(
+                        'Clear Users',
+                        style: CustomTextStyles.smallButtonText,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        store.dispatch(printUserPathThunk());
+                      },
+                      child: const Text(
+                        'Print Users Path',
+                        style: CustomTextStyles.smallButtonText,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
